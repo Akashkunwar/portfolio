f4b1fca1761c38ec4fc939b2ca424b3843b8d851
Akash Kumar <gnspda@gmail.com>
2022-12-17 15:41:30 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,15 +9,15 @@ function About() {
       <Header />
       <FadeIn className="about-container w-[85vw] mt-16 flex flex-col gap-5 font-extralight min-h-[65vh]" delay={400}>
         <div className="name text-3xl">I am Akash Kumar</div>
-        <div className="interest text-lg">a data science enthusiast</div>
+        <div className="interest text-lg">A data science enthusiast</div>
         <div className="education text-lg">
           currently a fresher at Indian Institute of Technology, Madras
         </div>
         <div className="location text-lg">
-          i am from Patna, capital of Bihar, state in India
+          Data is fun to work with, spend time with it and it will start telling stories.
         </div>
         <div className="interests text-lg">
-          my interest lies in machine learning and artificial intelligence
+          My interest lies in Machine learning and Artificial intelligence
         </div>
       </FadeIn>
     </div>
